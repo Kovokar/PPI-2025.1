6058b84eb31f0f63e9d701b4bbbfdaf7bc849bc5
pedro <pkovokar@gmail.com>
2025-04-24 23:01:44 -0300

    mudando tamanho intro

---
+++ teste/script.js
@@ -0,0 +1,12 @@
+
+document.getElementById('imagem').addEventListener('click', function() {
+    const img = this;
+
+    // Verifica se a imagem está com 100% de largura
+    console.log(img.style.width)
+    if (img.style.width === '60%') {
+        img.style.width = '10%';
+    } else {
+        img.style.width = '60%';
+    }
+});
