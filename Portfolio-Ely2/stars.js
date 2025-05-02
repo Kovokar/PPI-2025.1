8f3baff1bbae5825c8f4de8c1a4bbf617910802b
GiseleBCSantos <giselebianca57@gmail.com>
2025-05-01 22:09:33 -0300

    Update the stars effect to stars.js and stars.css

---
+++ Portfolio-Ely2/stars.js
@@ -0,0 +1,17 @@
+const addStarsBackground = () => {
+  const body = document.body;
+  const starsWrapper = document.createElement("section");
+  starsWrapper.className = "stars-wrapper";
+  const star1 = document.createElement("div");
+  star1.id = "stars1";
+  const star2 = document.createElement("div");
+  star2.id = "stars2";
+  const star3 = document.createElement("div");
+  star3.id = "stars3";
+  starsWrapper.appendChild(star1);
+  starsWrapper.appendChild(star2);
+  starsWrapper.appendChild(star3);
+  body.appendChild(starsWrapper);
+};
+
+addStarsBackground();
